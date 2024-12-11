d4dba93673b6f458a4b1e09c825d7eceba771d43
zoeznm <bomikim046@gmail.com>
2024-12-11 17:52:31 +0900

    엔드포인트 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Post, Get, Body, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Post('/chat')
+  async handlePostChat(@Body() body: { question: string }) {
+    const { question } = body;
+    if (!question) {
+      return { result: '질문을 입력해주세요.' };
+    }
+    return this.appService.getAIResponse(question);
+  }
+
   @Get()
-  async getAIResponse(@Query('question') question: string) {
+  async handleGetChat(@Query('question') question: string) {
     if (!question) {
       return { result: '질문을 입력해주세요.' };
     }
