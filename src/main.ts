237e6add356e048d42bf7e8fb85a3a915cd31161
zoeznm <bomikim046@gmail.com>
2024-12-11 17:52:12 +0900

    정적 파일 서빙

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use('/public', express.static(join(__dirname, '../public')));
+
   await app.listen(3000);
+  console.log('Application is running on: http://localhost:3000');
 }
 bootstrap();
