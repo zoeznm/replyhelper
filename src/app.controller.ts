f341462976376c87ddee413c6d319de7953201c9
zoeznm <bomikim046@gmail.com>
2024-12-11 13:39:39 +0900

    서비스 호출 컨트롤러

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,10 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getAIResponse(@Query('question') question: string) {
+    if (!question) {
+      return { result: '질문을 입력해주세요.' };
+    }
+    return this.appService.getAIResponse(question);
   }
 }
